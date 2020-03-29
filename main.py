27fc316756d4f82b55357754f32d728f30f183a9
Dylan Henderson <dylan.henderson@outlook.com>
2020-03-29 22:20:03 +1000

    Changed enemy_list to single list

--- main.py
+++ main.py
@@ -59,12 +59,9 @@ enemy3 = pygame.transform.scale(enemy3, (enemy_width, enemy_height))
 # spaceship object
 ship = Spaceship(ship_img, screen_width//2 - ship_width//2, screen_height-50, ship_width, ship_height, 5)
 
-# set up list of enemies
-enemies_list_1 = []
-enemies_list_2 = []
-enemies_list_3 = []
-start_x = 50
-increment = 50
+enemy_list = [[], [], []]   
+start_x = 50       # start at 50
+increment = 50     # increase x by 50
 speed = 2
 score_1 = 10
 score_2 = 20
@@ -72,13 +69,12 @@ score_3 = 30
 
 # create lists for every lvl enemy
 for i in range(10):
-    enemies_list_3.append(Enemy(enemy3, start_x, 50, enemy_width, enemy_height, speed, score_3))
-
-    enemies_list_2.append(Enemy(enemy2, start_x, 90, enemy_width, enemy_height, speed, score_2))
-    enemies_list_2.append(Enemy(enemy2, start_x, 130, enemy_width, enemy_height, speed, score_2))
-
-    enemies_list_1.append(Enemy(enemy1, start_x, 170, enemy_width, enemy_height, speed, score_1))
-    enemies_list_1.append(Enemy(enemy1, start_x, 210, enemy_width, enemy_height, speed, score_1))
+    # add 1 enemy to each row in column 1,2,3...,10
+    enemy_list[2].append(Enemy(enemy3, start_x, 50, enemy_width, enemy_height, speed, score_3))
+    enemy_list[1].append(Enemy(enemy2, start_x, 90, enemy_width, enemy_height, speed, score_2))
+    enemy_list[1].append(Enemy(enemy2, start_x, 130, enemy_width, enemy_height, speed, score_2))
+    enemy_list[0].append(Enemy(enemy1, start_x, 210, enemy_width, enemy_height, speed, score_1))
+    enemy_list[0].append(Enemy(enemy1, start_x, 170, enemy_width, enemy_height, speed, score_1))
     start_x += increment
 
 
@@ -100,74 +96,74 @@ def write(text, location=(10, 10), color=(255, 255, 255)):
 
 def update_enemies():
     """Updates location and velocity of enemies"""
-    for en in enemies_list_1:
+    for en in enemy_list[0]:
         en.x += en.velocity
         en.draw_enemy(screen)
 
     # move and draw every lvl 2 enemy
-    for en in enemies_list_2:
+    for en in enemy_list[1]:
         en.x += en.velocity
         en.draw_enemy(screen)
 
     # move and draw every lvl 3 enemy
-    for en in enemies_list_3:
+    for en in enemy_list[2]:
         en.x += en.velocity
         en.draw_enemy(screen)
 
+    # for every lvl 1 enemy
+    for enemy in enemy_list[0]:
+        # if one of them hit wall move every single enemy in each list
+        if (enemy.x >= screen_width - enemy.width) or (enemy.x <= 2):
+            for en in enemy_list[0]:
+                en.velocity = -en.velocity
+                en.y += 30
+                en.x += en.velocity
 
-    # for all lvl 1 enemies
-    for e in enemies_list_1:
-        if (e.x >= screen_width - e.width) or (e.x <= 2):
-            for a in enemies_list_1:
-                a.velocity = -a.velocity
-                a.y += 30
-                a.x += a.velocity
+            for en in enemy_list[1]:
+                en.velocity = -en.velocity
+                en.y += 30
+                en.x += en.velocity
 
-            for b in enemies_list_2:
-                b.velocity = -b.velocity
-                b.y += 30
-                b.x += b.velocity
+            for en in enemy_list[2]:
+                en.velocity = -en.velocity
+                en.y += 30
+                en.x += en.velocity
 
-            for c in enemies_list_3:
-                c.velocity = -c.velocity
-                c.y += 30
-                c.x += c.velocity
+    # for all lvl 2 enemies, if 1 hits the side, move every enemy
+    for enemy in enemy_list[1]:
+        if (enemy.x >= screen_width - enemy.width) or (enemy.x <= 2):
+            for en in enemy_list[0]:
+                en.velocity = -en.velocity
+                en.y += 30
+                en.x += en.velocity
 
-    # for all lvl 2 enemies
-    for en in enemies_list_2:
-        if (en.x >= screen_width - en.width) or (en.x <= 2):
-            for a in enemies_list_1:
-                a.velocity = -a.velocity
-                a.y += 30
-                a.x += a.velocity
+            for en in enemy_list[1]:
+                en.velocity = -en.velocity
+                en.y += 30
+                en.x += en.velocity
 
-            for b in enemies_list_2:
-                b.velocity = -b.velocity
-                b.y += 30
-                b.x += b.velocity
+            for en in enemy_list[2]:
+                en.velocity = -en.velocity
+                en.y += 30
+                en.x += en.velocity
 
-            for c in enemies_list_3:
-                c.velocity = -c.velocity
-                c.y += 30
-                c.x += c.velocity
+    # for all lvl 3 enemies, if 1 hits the side, move every enemy
+    for enemy in enemy_list[2]:
+        if (enemy.x >= screen_width - enemy.width) or (enemy.x <= 2):
+            for en in enemy_list[0]:
+                en.velocity = -en.velocity
+                en.y += 30
+                en.x += en.velocity
 
-    # for all lvl 3 enemies
-    for enem in enemies_list_3:
-        if (enem.x >= screen_width - enem.width) or (enem.x <= 2):
-            for a in enemies_list_1:
-                a.velocity = -a.velocity
-                a.y += 30
-                a.x += a.velocity
+            for en in enemy_list[1]:
+                en.velocity = -en.velocity
+                en.y += 30
+                en.x += en.velocity
 
-            for b in enemies_list_2:
-                b.velocity = -b.velocity
-                b.y += 30
-                b.x += b.velocity
-
-            for c in enemies_list_3:
-                c.velocity = -c.velocity
-                c.y += 30
-                c.x += c.velocity
+            for en in enemy_list[2]:
+                en.velocity = -en.velocity
+                en.y += 30
+                en.x += en.velocity
 
 
 # main game loop
@@ -204,36 +200,35 @@ while running:
             bullet.move_projectiles()
         else:
             bullets.pop(bullets.index(bullet))      # delete bullet from screen
-
         bullet.draw_projectiles(screen)
 
     # checks collision between bullets and enemies
     for b in bullets:
-        for e in enemies_list_1:
+        for e in enemy_list[0]:
             # if bullet x in between left and right of enemy hitbox
             if (b.x < (e.hitbox[0] + e.hitbox[2])) and b.x > e.hitbox[0]:
                 # if bullet y in between bottom and top of hitbox
                 if (b.y < e.hitbox[1] + e.hitbox[3]) and b.y > e.hitbox[1]:
                     bullets.pop(bullets.index(b))   # delete bullet from screen
-                    enemies_list_1.pop(enemies_list_1.index(e))   # delete enemy from screen
+                    enemy_list[0].pop(enemy_list[0].index(e))   # delete enemy from screen
                     score += score_1
 
-        for en in enemies_list_2:
+        for en in enemy_list[1]:
             # if bullet x in between left and right of enemy hitbox
             if (b.x < (en.hitbox[0] + en.hitbox[2])) and b.x > en.hitbox[0]:
                 # if bullet y in between bottom and top of hitbox
                 if (b.y < en.hitbox[1] + en.hitbox[3]) and b.y > en.hitbox[1]:
                     bullets.pop(bullets.index(b))   # delete bullet from screen
-                    enemies_list_2.pop(enemies_list_2.index(en))   # delete enemy from screen
+                    enemy_list[1].pop(enemy_list[1].index(en))   # delete enemy from screen
                     score += score_2
 
-        for enem in enemies_list_3:
+        for enem in enemy_list[2]:
             # if bullet x in between left and right of enemy hitbox
             if (b.x < (enem.hitbox[0] + enem.hitbox[2])) and b.x > enem.hitbox[0]:
                 # if bullet y in between bottom and top of hitbox
                 if (b.y < enem.hitbox[1] + enem.hitbox[3]) and b.y > enem.hitbox[1]:
                     bullets.pop(bullets.index(b))   # delete bullet from screen
-                    enemies_list_3.pop(enemies_list_3.index(enem))   # delete enemy from screen
+                    enemy_list[2].pop(enemy_list[2].index(enem))   # delete enemy from screen
                     score += score_3
 
     update_enemies()
